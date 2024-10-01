09399b7ab0cbd223819a93d5b387068d40a7b2f8
MASE98 <marco_sanchez98@hotmail.com>
2024-10-02 01:19:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+//Funcion copiar URL
+function copiarURL() {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url).then(() => {
+        alert('URL copiada al portapapeles');
+    }).catch(err => {
+        console.error('Error al copiar la URL: ', err);
+    });
+}
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+// Añadir evento de submit al formulario
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario recargue la página
+    
+    // Llamada para enviar los datos usando EmailJS
+    emailjs.send("service_czct4bn","template_prueba");
+  });
